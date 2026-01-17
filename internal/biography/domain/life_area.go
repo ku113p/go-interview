71f5bb79709fbf5ab86717c03b622e7b93ee6ba2
ku113p <ku113p@gmail.com>
2026-01-17 16:49:03 +0800

    add biography service

--- internal/biography/domain/life_area.go
+++ internal/biography/domain/life_area.go
@@ -8,13 +8,23 @@ type LifeArea struct {
 	common.UpdatableEntity
 	Title common.Title
 	Goal  common.Goal
+
+	Criteria []*Criterion
 }
 
 func NewLifeArea(title common.Title, goal common.Goal) LifeArea {
 	lifeArea := LifeArea{
-		Title: title,
-		Goal:  goal,
+		Title:    title,
+		Goal:     goal,
+		Criteria: make([]*Criterion, 0),
 	}
 	common.InitUpdatableEntity(&lifeArea.UpdatableEntity)
 	return lifeArea
+}
+
+func (la *LifeArea) AddCriteria(descs ...common.Description) {
+	for _, desc := range descs {
+		criterion := NewCriterion(desc)
+		la.Criteria = append(la.Criteria, &criterion)
+	}
 }
