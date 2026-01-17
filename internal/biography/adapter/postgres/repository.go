9fa2bc969c882a88732d6aba1e17318297e85a12
ku113p <ku113p@gmail.com>
2026-01-18 01:38:30 +0800

    ref - less repository, more mappers

--- internal/biography/adapter/postgres/repository.go
+++ internal/biography/adapter/postgres/repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go-interview/internal/biography/domain"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -46,4 +47,50 @@ func (r *Repository) Save(ctx context.Context, area *domain.LifeArea) error {
 	}
 
 	return tx.Commit(ctx)
+}
+
+func (r *Repository) GetByID(ctx context.Context, id uuid.UUID) (*domain.LifeArea, error) {
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback(ctx)
+
+	var lifeAreaSQL LifeAreaSQL
+	if err := tx.QueryRow(ctx, `
+		SELECT * FROM nodes WHERE id = $1
+	`, id).Scan(
+		&lifeAreaSQL.ID,
+		&lifeAreaSQL.ParentID,
+		&lifeAreaSQL.UserID,
+		&lifeAreaSQL.CreatedAt,
+		&lifeAreaSQL.UpdatedAt,
+		&lifeAreaSQL.Title,
+		&lifeAreaSQL.Goal,
+	); err != nil {
+		return nil, fmt.Errorf("get node: %w", err)
+	}
+	area := lifeAreaSQL.ToDomain()
+	rows, err := tx.Query(ctx, `
+		SELECT * FROM criteria WHERE node_id = $1
+	`, id)
+	if err != nil {
+		return nil, fmt.Errorf("get criteria: %w", err)
+	}
+	for rows.Next() {
+		var criterionSQL CriterionSQL
+		if err := rows.Scan(
+			&criterionSQL.ID,
+			&criterionSQL.NodeID,
+			&criterionSQL.CreatedAT,
+			&criterionSQL.UpdatedAt,
+			&criterionSQL.Description,
+			&criterionSQL.IsCompleted,
+		); err != nil {
+			return nil, fmt.Errorf("scan criterion: %w", err)
+		}
+		area.Criteria = append(area.Criteria, criterionSQL.ToDomain())
+	}
+
+	return area, nil
 }
